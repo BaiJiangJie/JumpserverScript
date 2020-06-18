3db36ccf5f882d9d47a3f487d6fc9a108f178957
Bai <bugatti_it@163.com>
2020-06-18 18:27:11 +0800

    [Update] 创建资产授权(1)

--- script_for_perms_create_v2/script/main.py
+++ script_for_perms_create_v2/script/main.py
@@ -1,14 +1,39 @@
+import os
 import sys
+import yaml
+
+
+class Interactor(object):
+
+    def __init__(self):
+        pass
+
+    def send(self, msg):
+        print(msg)
+
+    def receive(self):
+        pass
+
+
+interactor = Interactor()
 
 
 class Config(object):
 
     def __init__(self, file_path):
         self.file_path = file_path
-        self.log_file_path = './execute.log'
+        self.raw = None
+        self.log_file_path = None
+        self.init()
 
     def init(self):
-        pass
+        if not self.file_path.endswith('.yml'):
+            interactor.send('config file format error: {}'.format(self.file_path))
+            sys.exit(0)
+        if not os.path.isfile(self.file_path):
+            interactor.send('config file is not file: {}'.format(self.file_path))
+            sys.exit(0)
+        self.raw = yaml.load(self.file_path)
 
 
 class Logger(object):
@@ -22,18 +47,6 @@ class Logger(object):
         return f
 
     def write(self):
-        pass
-
-
-class Interactor(object):
-
-    def __init__(self):
-        pass
-
-    def send(self, msg):
-        pass
-
-    def receive(self):
         pass
 
 
@@ -62,9 +75,14 @@ class Executor(object):
 
 
 if __name__ == '__main__':
-    config = Config()
+    config_file_path = '../config_example.yml'
+
+    args = sys.argv
+    if len(args) >= 2:
+        config_file_path = args[1]
+
+    config = Config(file_path=config_file_path)
     logger = Logger()
     requester = Requester()
-    interactor = Interactor()
     executor = Executor()
     executor.execute()
