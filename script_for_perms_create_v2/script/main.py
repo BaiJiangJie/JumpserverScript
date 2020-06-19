70fe23ecaa578f95639295595fc1b14bde694a3c
Bai <bugatti_it@163.com>
2020-06-19 15:08:06 +0800

    [Update] 创建资产授权(6)

--- script_for_perms_create_v2/script/main.py
+++ script_for_perms_create_v2/script/main.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import yaml
+import time
 import requests
 from urllib.parse import urljoin
 
@@ -9,7 +10,8 @@ class API(object):
     api_map = {
         'api_org_list': 'api/v1/orgs/orgs/',
         'api_org_detail': 'api/v1/orgs/orgs/{org_id}/',
-        'api_users_detail': 'api/v1/users/users/'
+        'api_users_list': 'api/v1/users/users/',
+        'api_system_users_list': 'api/v1/assets/system-users/',
     }
 
     def __getattr__(self, item):
@@ -52,18 +54,23 @@ class Interactor(object):
         The script flow is as follows:\n
         1. Enter the name of the organization\n
         2. Enter the user's username\n
-        3. Enter the username of the system user\n
+        3. Enter the name of the system user\n
         4. Enter the asset's hostname or enter assets CSV file path\n
         5. Create asset permission\n
         
         '''
         self.send(description)
 
+    def exit(self):
+        self.send_to_user_info('Exit')
+        sys.exit(0)
+
     def ask_user_if_next_step(self):
         opt = self.receive('Go on (Y/N): ')
         if opt in ['N', 'n']:
-            self.send('Exit')
-            sys.exit(0)
+            return False
+        else:
+            return True
 
     def ask_user_for_org_name(self):
         opt = self.receive('Input Organization name: ')
@@ -71,6 +78,10 @@ class Interactor(object):
 
     def ask_user_for_user_username(self):
         opt = self.receive("Input user's username: ")
+        return opt
+
+    def ask_user_for_system_user_name(self):
+        opt = self.receive("Input system user's name: ")
         return opt
 
     def ask_user_if_continue_add(self, resource):
@@ -142,9 +153,9 @@ class Requester(object):
             'Content-Type': 'application/json'
         })
 
-    def init_headers_jumpserver_org(self, org_name=None, org_id=None):
+    def add_jumpserver_org_to_headers(self, org_name=None, org_id=None):
         self.headers.update({
-            'X-JMS-ORG': org_name
+            'X-JMS-ORG': org_id if org_id is not None else org_name
         })
 
     def get(self, url, params=None):
@@ -169,7 +180,9 @@ class Executor(object):
         self.users_username = []
         self.users = []
 
-        self.system_users_username = []
+        self.system_users_name = []
+        self.system_users = []
+
         self.assets_hostname = []
 
     def get_org_list(self):
@@ -185,24 +198,26 @@ class Executor(object):
                 return True, org
         return False, None
 
-    def wait_and_check_for_user_input_org_name(self):
+    def wait_for_user_input_and_check_org_name(self):
         while True:
             org_name = interactor.ask_user_for_org_name()
             if org_name.lower() == 'default':
-                self.org_name = org_name.upper()
-                self.org = {'name': self.org_name}
+                org_name = org_name.upper()
+                org = {'name': org_name, 'id': None}
                 break
 
             exist, org = self.check_org_name_exist(org_name)
             if exist:
-                self.org_name = org_name
-                self.org = org
-                interactor.send_to_user_info('Organization exist: {}'.format(self.org_name))
+                interactor.send_to_user_info('Organization exist: {}'.format(org_name))
                 break
-            interactor.send_to_user_error('Organization not exist: {}'.format(self.org_name))
+
+            interactor.send_to_user_error('Organization not exist: {}'.format(org_name))
+
+        self.org_name = org_name
+        self.org = org
 
     def check_user_username_exist(self, username):
-        url = jumpserver_api.api_users_detail
+        url = jumpserver_api.api_users_list
         params = {
             'username': username
         }
@@ -212,41 +227,113 @@ class Executor(object):
             if len(users) == 1:
                 return True, users[0]
             else:
-                interactor.send_to_user_error('Exist many to for username: {}'.format(username))
                 return False, None
         else:
             return False, None
 
-    def wait_and_check_for_user_input_users_username(self):
+    def wait_for_user_input_and_check_users_username(self):
+        users_username = []
+        users = []
+
         while True:
-            interactor.send_to_user_info('Current select users: {}'.format(self.users_username))
+            if len(users_username) >= 1:
+                interactor.send_to_user_info('Current select users: {}'.format(users_username))
             user_username = interactor.ask_user_for_user_username()
             exist, user = self.check_user_username_exist(user_username)
             if exist:
                 interactor.send_to_user_info('User exist: {}'.format(user_username))
-                if user_username not in self.users_username:
-                    self.users_username.append(user_username)
-                    self.users.append(user)
+                if user_username not in users_username:
+                    users_username.append(user_username)
+                    users.append(user)
                 if interactor.ask_user_if_continue_add('user'):
                     continue
                 else:
                     break
             else:
                 interactor.send_to_user_error('User not exist: {}'.format(user_username))
-                if len(self.users) >= 1:
+                if len(users) >= 1:
                     if interactor.ask_user_if_continue_add('user'):
                         continue
                     else:
                         break
+        self.users_username = users_username
+        self.users = users
+        interactor.send_to_user_info('Current select users: {}'.format(users_username))
 
-    def wait_for_user_input_system_users_username(self):
-        pass
+    def check_system_user_name_exist(self, name):
+        url = jumpserver_api.api_system_users_list
+        params = {
+            'name': name
+        }
+        res = requester.get(url, params=params)
+        if res.status_code == 200:
+            system_users = res.json()
+            if len(system_users) == 1:
+                return True, system_users[0]
+            else:
+                return False, None
+        else:
+            return False, None
+
+    def wait_for_user_input_and_check_system_users_name(self):
+        system_users_name = []
+        system_users = []
+
+        while True:
+            if len(system_users_name) >= 1:
+                interactor.send_to_user_info('Current select system users: {}'.format(system_users_name))
+            system_user_name = interactor.ask_user_for_system_user_name()
+            exist, system_user = self.check_system_user_name_exist(system_user_name)
+            if exist:
+                interactor.send_to_user_info('System user exist: {}'.format(system_user_name))
+                if system_user_name not in system_users_name:
+                    system_users_name.append(system_user_name)
+                    system_users.append(system_user)
+                if interactor.ask_user_if_continue_add('system user'):
+                    continue
+                else:
+                    break
+            else:
+                interactor.send_to_user_error('System user not exist: {}'.format(system_user_name))
+                if len(system_users) >= 1:
+                    if interactor.ask_user_if_continue_add('system user'):
+                        continue
+                    else:
+                        break
+
+        self.system_users_name = system_users_name
+        self.system_users = system_users
+        interactor.send_to_user_info('Current select system users: {}'.format(system_users_name))
 
     def wait_for_user_input_assets_hostname(self):
         pass
 
-    def show_data_of_create_permission(self):
-        pass
+    def show_data_of_will_create_permission(self):
+        asset_permission_name = '{prefix}_{timestamp}'.format(
+            prefix=config.permissions_name_prefix,
+            timestamp=int(time.time())
+        )
+        description = '''
+        Asset permission will be created using the following data:
+        
+        Asset permission name: {asset_permission_name}
+        
+        Organization: {org}
+        
+        Users: {users}
+        
+        System users: {system_users}
+        
+        Assets: {assets}
+        '''
+        description = description.format(
+            asset_permission_name=asset_permission_name,
+            org=self.org_name,
+            users=', '.join(self.users_username),
+            system_users=', '.join(self.system_users_name),
+            assets=', '.join(self.assets_hostname)
+        )
+        interactor.send(description)
 
     def show_data_for_create_permission_result(self):
         pass
@@ -258,13 +345,14 @@ class Executor(object):
         pass
 
     def execute(self):
-        self.wait_and_check_for_user_input_org_name()
-        requester.init_headers_jumpserver_org(self.org_name)
-        self.wait_and_check_for_user_input_users_username()
-        self.wait_for_user_input_system_users_username()
+        self.wait_for_user_input_and_check_org_name()
+        requester.add_jumpserver_org_to_headers(self.org_name, self.org['id'])
+        self.wait_for_user_input_and_check_users_username()
+        self.wait_for_user_input_and_check_system_users_name()
         self.wait_for_user_input_assets_hostname()
-        self.show_data_of_create_permission()
-        interactor.ask_user_if_next_step()
+        self.show_data_of_will_create_permission()
+        if not interactor.ask_user_if_next_step():
+            interactor.exit()
         self.show_data_for_create_permission_result()
         self.ask_user_if_continue_create_permission()
         self.show_summary_for_the_execute()
@@ -281,7 +369,8 @@ if __name__ == '__main__':
     config.check()
 
     interactor.send_to_user_the_script_description()
-    interactor.ask_user_if_next_step()
+    if not interactor.ask_user_if_next_step():
+        interactor.exit()
 
     executor = Executor()
     executor.execute()
